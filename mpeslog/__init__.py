6011abbd0255fe55956dfafb2fe880b7ba27405a
RealPolitiX <xrpatrick@gmail.com>
2019-04-21 08:35:19 +0200

    Added template

--- mpeslog/__init__.py
+++ mpeslog/__init__.py
@@ -1,1 +1,6 @@
 from .log import *
+
+__version_info__ = ('0', '1', '0')
+__version__ = '.'.join(__version_info__)
+__author__ = 'R. Patrick Xian'
+__all__ = []
---
+++ mpeslog/template.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@author: R. Patrick Xian
+"""
+
+# ================================================================= #
+# Example parameter settings for band structure mapping experiments #
+# ================================================================= #
+
+from __future__ import print_function, division
+import numpy as np
+import h5py as h5
+import silx.io as silo
+
+
+def write_template(filename, save_addr):
+    """ Write a template of metadata.
+    """
+
+    return
+
+
+def load_template(file_addr):
+    """ Read an existing metadata template.
+    """
+
+    return
